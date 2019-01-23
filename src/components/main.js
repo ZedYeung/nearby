a1f784f2fbede195eab5c822231813377a3b155b
Yue (Zed) Yang <shoominga@gmail.com>
2019-01-24 03:28:35 +0800

    add main

---
+++ src/components/main.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Register} from "./Register"
+import {Login} from "./Login"
+import {Switch, Route, Redirect} from 'react-router-dom'
+import {Home} from './Home'
+
+export class Main extends React.Component{
+    getLogin = () => {
+        return this.props.isLoggedIn? <Redirect to="/home"/> : <Login handleLogin = {this.props.handleLogin}/>;
+    }
+
+    getHome = () => {
+        return this.props.isLoggedIn? <Home/> : <Redirect to="/login"/>;
+    }
+
+    getRoot = () => {
+        return <Redirect to ='/login'/>
+    }
+
+    render(){
+        return (
+            <section className="main">
+                <Switch>
+                    <Route exact path="/" render={this.getRoot}/>
+                    <Route path="/register" component={Register}/>
+                    <Route path="/login" render={this.getLogin}/>
+                    <Route path="/home" render={this.getHome}/>
+                    <Route render={this.getRoot}/>
+                </Switch>
+            </section>
+        )
+    }
+}
