df6e40eb49f3ae04b7f4d27b6c806e67c19489be
Yue (Zed) Yang <shoominga@gmail.com>
2019-01-24 03:27:55 +0800

    add Header

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from "react"
+import logo from '../assets/images/logo.svg';
+import {Icon} from 'antd';
+import PropTypes from 'prop-types';
+
+export class Header extends React.Component{
+    static propTypes = {
+        isLoggedIn : PropTypes.bool.isRequired,
+        handleLogout: PropTypes.func.isRequired,
+    }
+
+    render(){
+        return (
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Nearby</h1>
+                {
+                    this.props.isLoggedIn &&
+                        <a href=""
+                           className="logout"
+                            onClick={this.props.handleLogout}>
+                            <Icon type="logout"/>{' '}logout
+                        </a>
+                }
+            </header>
+        )
+    }
+}
