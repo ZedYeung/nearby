fc36c9acd3c25dbe2af5a11ac367240f458ee85b
Yue (Zed) Yang <shoominga@gmail.com>
2019-03-05 09:37:19 -0800

    use gcr.io for docker

--- frontend/run.sh
+++ frontend/run.sh
@@ -2,6 +2,6 @@
 set -a
 source .env.development
 echo "build..."
-docker build -t ${DOCKER_REGISTRY}/${DOCKER_USER}/${APP}_frontend .
+docker build -t ${DOCKER_REGISTRY}/${GCP_PROJECT_ID}/${APP}_frontend .
 echo "run..."
-docker run -it --rm -e "BACKEND=${BACKEND}" -p ${PORT}:80 ${DOCKER_REGISTRY}/${DOCKER_USER}/${APP}_frontend
+docker run -it --rm -e "BACKEND=${BACKEND}" -p ${PORT}:80 ${DOCKER_REGISTRY}/${GCP_PROJECT_ID}/${APP}_frontend
